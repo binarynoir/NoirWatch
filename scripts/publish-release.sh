3be930344cb9c42ad73d92ff36f9e0dc438b26fe
KryptonianSon <kryptonianson@users.noreply.github.com>
2024-11-29 12:35:52 -0500

    Changed: Publish script

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Extract the version number from the noirwatch script
-VERSION=$(grep -Eo 'VERSION=[0-9]+\.[0-9]+\.[0-9]+' noirwatch | cut -d '=' -f 2)
+APP_SCRIPT="noirwatch"
+
+# Extract the version number from the application script
+VERSION=$(grep -Eo 'VERSION=[0-9]+\.[0-9]+\.[0-9]+' $APP_SCRIPT | cut -d '=' -f 2)
 
 # Check if the version number was found
 if [ -z "$VERSION" ]; then
-    echo "Version number not found in noirwatch script."
+    echo "Version number not found in $APP_SCRIPT script."
     exit 1
 fi
-
-REPO="binarynoir/noirwatch"
 
 # Ensure repo is up-to-date
 git pull
